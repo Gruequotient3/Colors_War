b9d4a2359a492ec8648d4f884cbad0758613c4f1
LEVASTRE Clément <levastre.clement@gmail.com>
2025-04-11 11:03:53 +0200

    Add Texture class

---
+++ src/gfx/texture.hpp
@@ -0,0 +1,24 @@
+#ifndef TEXTURE_HPP
+#define TEXTURE_HPP
+
+
+#include <glad/glad.h>
+#include <string>
+
+class Texture{
+    public:
+        unsigned int id;
+        GLenum type;
+        int width;
+        int height;
+    
+        Texture();
+        Texture(const char *imagePath, GLenum format);
+    
+        void LoadTexture(const char *imagePath, GLenum format);
+
+
+};
+
+
+#endif
